1857561b85a0eca71e7527204ba4606011133d46
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2023-09-07 09:06:00 +0200

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -34,7 +34,77 @@ myIdSearchButton.addEventListener("click", () => {
 
 // fetch functions --------------------------------------------------------------------
 // your code goes here
+async function fetchMealsByFirstLetter(letter) {
+  try {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`);
 
+    if (!response.ok) {
+      throw new Error(`Network response was not ok (status: ${response.status})`);
+    }
+
+    const data = await response.json();
+
+    if (data && data.meals) {
+      return data;
+    } else {
+      throw new Error('No meals found for the provided letter');
+    }
+  } catch (error) {
+    console.error('Fejl ved at hente data:', error);
+    return { error: 'Der opstod en fejl ved hentning af data.' };
+  }
+}
+
+
+async function getRecipiesByName(name) {
+  try {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Fejl ved at hente data:', error);
+    return { error: 'Der opstod en fejl ved hentning af data.' };
+  }
+}
+
+async function fetchMealById(id) {
+  try {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Fejl ved at hente data:', error);
+    return { error: 'Der opstod en fejl ved hentning af data.' };
+  }
+}
+
+// Opdater resultaterne, når søgeknappen klikkes
+myfirstLetterSearchButton.addEventListener("click", async () => {
+  serchMode = "firstLetterSearch";
+  const letter = myfirstLetterInput.value;
+  console.info(letter);
+
+  const result = await fetchMealsByFirstLetter(letter);
+  setupResultView(result);
+});
+
+myNameSearchButton.addEventListener("click", async () => {
+  serchMode = "nameSearch";
+  const name = myNameInput.value;
+  console.info(name);
+
+  const result = await getRecipiesByName(name);
+  setupResultView(result);
+});
+
+myIdSearchButton.addEventListener("click", async () => {
+  serchMode = "idSearch";
+  const id = myIdInput.value;
+  console.info(id);
+
+  const result = await fetchMealById(id);
+  setupResultView(result);
+});
 
 
 
@@ -48,23 +118,75 @@ function setupResultView(myData) {
   switch (serchMode) {
     case "firstLetterSearch":
       console.log(myData);
-      // do view stuff with the data here
+
+      // Clear previous results
+      myResultElement.innerHTML = '';
+
+      // Check if there are meals to display
+      if (myData.meals && myData.meals.length > 0) {
+        const mealList = document.createElement('ul');
+
+        myData.meals.forEach((meal) => {
+          const listItem = document.createElement('li');
+          listItem.textContent = meal.strMeal;
+          mealList.appendChild(listItem);
+        });
+
+        myResultElement.appendChild(mealList);
+      } else {
+        // Handle case when no meals are found
+        myResultElement.textContent = 'No meals found for the first letter.';
+      }
+      break;
       break;
 
     case "nameSearch":
       console.log(myData.meals);
-      let myText = "";
 
-      myData.meals.map((myMeal) => {
-        myText += myMeal.strMeal + ", ";
-      });
+      // Clear previous results
+      myResultElement.innerHTML = '';
 
-      myResultElement.textContent = myText;
+      // Check if there are meals to display
+      if (myData.meals && myData.meals.length > 0) {
+        const mealList = document.createElement('ul');
+
+        myData.meals.forEach((myMeal) => {
+          const listItem = document.createElement('li');
+          listItem.textContent = myMeal.strMeal;
+          mealList.appendChild(listItem);
+        });
+
+        myResultElement.appendChild(mealList);
+      } else {
+        // Handle case when no meals are found
+        myResultElement.textContent = 'No meals found with the provided name.';
+      }
       break;
 
     case "idSearch":
       console.log(myData);
-      // do view stuff with the data here
+
+      // Clear previous results
+      myResultElement.innerHTML = '';
+
+      // Check if a meal is found
+      if (myData.meals && myData.meals.length > 0) {
+        const meal = myData.meals[0]; // Assuming only one meal is returned
+
+        // Create a div to display the meal details
+        const mealDetailsDiv = document.createElement('div');
+        mealDetailsDiv.innerHTML = `
+            <h3>${meal.strMeal}</h3>
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+            <p>Category: ${meal.strCategory}</p>
+            <p>Area: ${meal.strArea}</p>
+          `;
+
+        myResultElement.appendChild(mealDetailsDiv);
+      } else {
+        // Handle case when no meal is found
+        myResultElement.textContent = 'No meal found with the provided ID.';
+      }
       break;
 
     case "errorMessage":
